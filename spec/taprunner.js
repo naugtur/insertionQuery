1381957001f9a695e5fb7ef1f1952f72c917593e
Naugtur <naugtur@gmail.com>
2013-03-14 19:38:01 +0100

    maybe this time... #2

--- spec/taprunner.js
+++ spec/taprunner.js
@@ -1,3 +1,15 @@
+function mkglob(obj){
+	for(var i in obj){
+		window[i]=obj[i];
+	}
+}
+
+mkglob(require("http://pivotal.github.com/jasmine/lib/jasmine.js"));
+require("https://raw.github.com/larrymyers/jasmine-reporters/master/src/jasmine.tap_reporter.js"); 
+mkglob(require("http://naugtur.github.com/insertionQuery/insQ.js"));
+require("http://naugtur.github.com/insertionQuery/spec/test.js");
+
+
 var jasmineEnv = jasmine.getEnv();
 jasmineEnv.updateInterval = 250;
 
